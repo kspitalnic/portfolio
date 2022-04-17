c3e903828b407d30eaf45d2691a96829d7d95e05
Kate Spitalnic <kespitalnic@gmail.com>
2022-04-17 17:17:08 -0400

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 function Footer() {
   return (
-    <section className="my-5">
-      <h4 id="footer">Footer</h4>
-    </section>
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <a href="https://github.com/kspitalnic" target="_blank" rel="noreferrer">
+            GitHub 
+          </a>
+           || 
+                    <a
+            href="https://www.linkedin.com/in/kate-spitalnic-74bb90215/"
+            target="_blank" rel="noreferrer"
+          >
+             LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 }
 

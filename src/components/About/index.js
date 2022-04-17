26e1ef5b90940af0aaa755ad4e95899bfbbd621e
Kate Spitalnic <kespitalnic@gmail.com>
2022-04-17 01:54:50 -0400

    ability to download resume pdf

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,6 +10,10 @@ function About() {
             <h6>Katie Spitalnic's full stack web developer portfolio.</h6>
             <p>Katie Spitalnic is currently a student at UConn's Coding Bootcamp where she is learning skills to become a full-stack developer. Projects and updates will be uploaded here as Kate continues here web developer journey.
 </p>
+<span>
+          <a href={require("../../assets/KSpitalnicResume.pdf")} download>
+					Click here to download Kate's Resume.
+				</a>				</span>
         </div>
         </div>
     )

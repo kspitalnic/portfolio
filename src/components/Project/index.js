beb9c6a6a9f6b8aaebf3a64f84a2337dd5c72f0a
Kate Spitalnic <kespitalnic@gmail.com>
2022-04-17 16:27:48 -0400

    updated cards and adding project data

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,81 +1,85 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
+import React, { useState } from "react";
+import Card from 'react-bootstrap/Card';
 
-
-function Project() {
     const projects = [
         {
-            name: 'Project 1',
-            description: 'Project description',
+            id: 1,
+            name: 'XYZ Car-Rental Review',
+            description: 'Web application to view, create, edit, and update reviews on cars from XYZ car rentals.',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         },
         {
-            name: 'Project 2',
-            description: 'Project description',
+            id: 2,
+            name: 'Initial Portfolio',
+            description: 'Front end portfolio application displaying web development projects.',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         },
         {
-            name: 'Project 3',
-            description: 'Project description',
+            id: 3,
+            name: 'Easy Budget',
+            description: 'Budgeting tracking application that uses third party APIs to show amazon prices and display related giphys.',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         },
         {
-            name: 'Project 4',
-            description: 'Project description',
+            id: 4,
+            name: 'TYB Band Fanpage',
+            description: 'Fanpage front end application for Thousand Years Betwen Band',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         },
         {
+            id: 5,
             name: 'Project 5',
-            description: 'Project description',
+            description: 'Coming Soon',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         },
         {
+            id: 6,
             name: 'Project 6',
-            description: 'Project description',
+            description: 'Coming Soon',
             image: 'portrait',
             github: 'github link',
             deployed: 'live site link'
         }
     ];
+    function Project() {
     return (
         <div className="center flex-row cards">
             {projects.map((project) => (
+                <div>
                 <Card style={{ width: '18rem' }} className="card">
-  <Card.Img src={require(`../../assets/images/${project.image}.jpeg`)} alt="Project thumbnail"/>
+  <Card.Img src={require(`../../assets/images/${project.image}.jpeg`)} alt="Project thumbnail" />
   <Card.ImgOverlay>
                 <Card.Body>
-                  <Card.Title>{project.name}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{project.description}</Card.Subtitle>
+                  <Card.Title className="card-title" href={project.depoloyed}>{project.name}</Card.Title>
+                  <Card.Subtitle className="card-description mb-2">{project.description}</Card.Subtitle>
+                  <br />
+
                   <Card.Link href={project.github}>Deployed Application</Card.Link>
-                  <Card.Link href={project.github}>Project Repository</Card.Link>
+                  <br />
+                  <br />
+
+                  <Card.Link href={project.deployed}>Project Repository</Card.Link>
                 </Card.Body>
                 </Card.ImgOverlay>
 
               </Card>
-                // <div>
-                //     <a href={project.deployed} target="_blank" rel="noreferrer">
-                //         <img className="image" src={require(`../../assets/images/${project.image}.jpeg`)} alt="Project thumbnail" />
-                //     </a>
-                //     <p>{project.name}</p>
-                //     <p>{project.description}</p>
-                //     <p>{project.github}</p>
-                //     <p>{project.deployed}</p>
-                // </div>
+            
+                </div>
 
             ))}
         </div>
-    );
-}
+    );}
+
 export default Project;
 
 
